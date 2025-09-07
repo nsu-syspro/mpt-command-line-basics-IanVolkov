d8da54d6366ea7774c7150ec25044d8b3b1228a0
Ian Volkov <y.volkov@g.nsu.ru>
2025-09-07 17:45:02 +0700

    solution

--- solution/task2.sh
+++ solution/task2.sh
@@ -1,1 +1,1 @@
-find . -name "foo.txt" -or -name "bar.txt" -or -name "baz.txt" -exec cat {} \;
+find . -type f -and -name "foo.txt" -or -type f -and -name "bar.txt" -or -type f -and -name "baz.txt" -exec cat {} \;
