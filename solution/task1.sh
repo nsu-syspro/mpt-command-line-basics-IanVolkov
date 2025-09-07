de0ebed0506f3fe56c65759d82dd0f5d9815e31d
Ian Volkov <y.volkov@g.nsu.ru>
2025-09-07 23:25:09 +0700

    added mktemp

--- solution/task1.sh
+++ solution/task1.sh
@@ -1,5 +1,5 @@
-mkdir tmp
-tar -xf data/archive-part1.tar -C tmp
-unzip data/archive-part2.zip -d tmp
-tar -czf data/archive-combined.tar.gz -C tmp .
-rm -fr tmp
+tmp=$(mktemp -d)
+tar -xf data/archive-part1.tar -C $tmp
+unzip data/archive-part2.zip -d $tmp
+tar -czf data/archive-combined.tar.gz -C $tmp .
+rm -fr $tmp
